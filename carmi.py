40f83662fa877fafed8f877378a96e78249e862f
shellyw19-meet <shellyw19@meet.mit.edu>
2017-08-06 16:28:45 +0300

    Stopping here

---
+++ carmi.py
@@ -0,0 +1,20 @@
+food_list = ["hamburger", "pizza", "cola", "water"]
+rand_index = random.randint
+
+
+plane_food = 'aa'
+if plane.pos() == hamburger.pos:
+    plane_food = 'hamborger'
+    print('you picked up hamborger') 
+if plane.pos== cola.pos:
+    plane_food = 'cola'
+    print('you picked up cola') 
+if plane.pos == pizza.pos:
+    plane_food = 'pizza'
+    print('you picked up pizza') 
+if plane.pos == water.pos:
+    plane_food = 'water'
+    print('you picked up water')
+
+
+    
