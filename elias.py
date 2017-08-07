ec468716ce53ad6508df87483510829660863190
shellyw19-meet <shellyw19@meet.mit.edu>
2017-08-07 16:53:16 +0300

    Stopping here

--- elias.py
+++ elias.py
@@ -1,3 +1,4 @@
+
 import turtle
 import random 
 import time
@@ -6,11 +7,15 @@ size_x=800
 size_y=500
 turtle.setup(size_x,size_y)
 turtle.penup()
+#how far the snake moves 
 SQUARE_SIZE=20
 pos_list=[]
 plane=turtle.clone()
-plane.shape('square')
+#the shape of the plane
+turtle.register_shape("download (2).gif")
+plane.shape("download (2).gif")
 turtle.hideturtle()
+
 
 UP_ARROW='Up'
 LEFT_ARROW='Left'
@@ -22,9 +27,10 @@ UP=0
 DOWN=1
 LEFT=2
 RIGHT=3
+turn=4
 direction=UP
 end_time= time.time()+120
-
+############################################################################
 def up():
     global direction
     direction=UP
@@ -48,42 +54,51 @@ def left():
     direction=LEFT
     move_plane()
     print("you pressed LEFT ")
-s_l_s = []
+def turn():
+    global direction
+    direction=turn
+    turtle.right(90)
+
 turtle.onkeypress(up,UP_ARROW)
 turtle.onkeypress(right,RIGHT_ARROW)
 turtle.onkeypress(left,LEFT_ARROW)
 turtle.onkeypress(down,DOWN_ARROW)
 turtle.listen()
-clear_score = turtle.clone()
-clear_score.speed(10)
-clear_score.color('white')
-turtle.goto(400, 0)
-def move_plane():
+
+turtle.goto(200,0)#this is moing the turtle to 200 to write the timer 
+
+##################
+start_time = 120##chpse how much time till the game ends 
+##################
+ 
+def timer():#the game timer
+    global start_time
+    start_time = start_time-1
+    print(start_time)
+    turtle.clear()
+    turtle.write(start_time,font=50)
+    if start_time==0:
+        quit()
+        print("you ran out of time ")
+    turtle.ontimer(timer,1000)
+
+def move_plane():#how the plane moves 
     my_pos=plane.pos()
     x_pos=my_pos[0]
-    y_pos=my_pos[1]
-
-    while end_time-time.time()>0:
-        print(int(end_time-time.time()))
-        clear_score.goto(400, 0)
-        ax = clear_score.stamp()
-        s_l_s.append(ax)
-        clear_score.goto(800, 0)
-        turtle.write(int(end_time-time.time()),font=50)
+    y_pos=my_pos[1]       
+    if direction==RIGHT:
+        plane.goto(x_pos+SQUARE_SIZE,y_pos)
+        print('you moevd right ')
+    if direction==DOWN:
+        plane.goto(x_pos,y_pos-SQUARE_SIZE)
+        print('you moevd DOWN ')
+    if direction==LEFT:
+        plane.goto(x_pos-SQUARE_SIZE,y_pos)
+        print('you moevd LEFT ')
+    if direction==UP:
+        plane.goto(x_pos,y_pos+SQUARE_SIZE)
+        print('you moevd UP ')
         
+timer()#this is baisicly activating he timer 
 
-
-        if direction==RIGHT:
-            plane.goto(x_pos+SQUARE_SIZE,y_pos)
-            print('you moevd right ')
-        if direction==DOWN:
-            plane.goto(x_pos,y_pos-SQUARE_SIZE)
-            print('you moevd DOWN ')
-        if direction==LEFT:
-            plane.goto(x_pos-SQUARE_SIZE,y_pos)
-            print('you moevd LEFT ')
-        if direction==UP:
-            plane.goto(x_pos,y_pos+SQUARE_SIZE)
-            print('you moevd UP ')
-        
             
