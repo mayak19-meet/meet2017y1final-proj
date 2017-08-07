6b8bd0b0c417ee7f7ca4e94537a0e49e88e61722
shellyw19-meet <shellyw19@meet.mit.edu>
2017-08-07 12:27:42 +0300

    Stopping here

---
+++ shelly.py
@@ -0,0 +1,29 @@
+import turtle
+
+pizza = turtle.clone()
+hamburger = turtle.clone()
+water = turtle.clone()
+cola = turtle.clone()
+
+
+pizza = "Pizza.gif"
+
+turtle.register_shape(pizza)
+turtle.shape(pizza)
+
+hamburger = "burger_sandwich.gif"
+
+turtle.register_shape(hamburger)
+turtle.shape(hamburger)
+
+water = "water.gif"
+
+turtle.register_shape(water)
+turtle.shape(water)
+
+cola = "cocacola.gif"
+
+turtle.register_shape(cola)
+turtle.shape(cola)
+
+
