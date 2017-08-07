5cc0d79342b85857001df8197b61bda2b45ae1b3
shellyw19-meet <shellyw19@meet.mit.edu>
2017-08-07 11:27:27 +0200

    Stopping here

---
+++ maya.py
@@ -0,0 +1,21 @@
+import turtle
+
+kenya=turtle.clone()
+egypt=turtle.colne()
+uganda=turtle.colne()
+syria=turtle.colne()
+
+kenya="kenya1.jpg"
+turtle.register_shape(kenya)
+turtle.shape(kenya)
+                      
+egypt="egypt1.jpg"
+turtle.register_shape(egypt)
+turtle.shape(egypt)
+
+uganda="uganda1.jpg"
+turtle.register_shape(uganda)
+turtle.shape(uganda)
+
+syria="syria1.jpg"
+turtle.shape(syria)
