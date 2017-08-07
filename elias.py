1a0585d154ea22ced892198f9c23863394ad840d
shellyw19-meet <shellyw19@meet.mit.edu>
2017-08-07 12:30:51 +0300

    Stopping here

--- elias.py
+++ elias.py
@@ -1,123 +1,89 @@
 import turtle
-import random
-color=0
+import random 
+import time
 turtle.tracer(1,0)
-counter=1
-SIZE_X=800
-SIZE_Y=500
-turtle.setup(SIZE_X,SIZE_Y)
+size_x=800
+size_y=500
+turtle.setup(size_x,size_y)
 turtle.penup()
 SQUARE_SIZE=20
-turtle.color("red")
+pos_list=[]
 plane=turtle.clone()
-plane.shape('circle')
+plane.shape('square')
 turtle.hideturtle()
-plane.color("green")
 
-
-
-
-    
-    
-
-    
-UP_ARROW="Up"
-LEFT_ARROW="Left"
-DOWN_ARROW="Down"
-RIGHT_ARROW="Right"
+UP_ARROW='Up'
+LEFT_ARROW='Left'
+RIGHT_ARROW='Right'
+DOWN_ARROW='Down'
 TIME_STEP=100
-SPACEBAR="space"
+SPACEBAR='spacebar'
 UP=0
 DOWN=1
 LEFT=2
 RIGHT=3
-    
 direction=UP
-
-UP_EDGE=250
-DOWN_EDGE=-250
-RIGHT_EDGE=400
-LEFT_EDGE=-400
+end_time= time.time()+120
 
 def up():
     global direction
-    if direction !=DOWN:
-        direction=UP
-    print("you moved up !!")
-    
+    direction=UP
+    move_plane()
+    print("you pressed up ")
+
 def down():
     global direction
-    if direction != UP:
-        direction=DOWN
-    print("you moved down !!")
-
-def left():
-    global direction
-    if direction !=RIGHT:
-        direction=LEFT
-    print("you moved left !! ")
+    direction=DOWN
+    move_plane()
+    print("you pressed DOWN ")
 
 def right():
     global direction
-    if direction !=LEFT:
-        direction=RIGHT
-    print("you moved right !!")
+    direction=RIGHT
+    move_plane()
+    print("you pressed RIGHT ")
 
+def left():
+    global direction
+    direction=LEFT
+    move_plane()
+    print("you pressed LEFT ")
+s_l_s = []
 turtle.onkeypress(up,UP_ARROW)
-turtle.onkeypress(down,DOWN_ARROW)
-turtle.onkeypress(left,LEFT_ARROW)
 turtle.onkeypress(right,RIGHT_ARROW)
+turtle.onkeypress(left,LEFT_ARROW)
+turtle.onkeypress(down,DOWN_ARROW)
 turtle.listen()
-
-
-
-    
-    
-def move_snake():
+clear_score = turtle.clone()
+clear_score.speed(10)
+clear_score.color('white')
+turtle.goto(400, 0)
+def move_plane():
     my_pos=plane.pos()
     x_pos=my_pos[0]
     y_pos=my_pos[1]
-    if direction==RIGHT:
-        plane.goto(x_pos+SQUARE_SIZE,y_pos)
-        print("you moved right")
-    elif direction==LEFT:
-        plane.goto(x_pos-SQUARE_SIZE,y_pos)
-        print("you moved left !!")
-    elif direction==UP:
-        plane.goto(x_pos,y_pos+SQUARE_SIZE)
-        print("you moved up !!")
-    elif direction==DOWN:
-        plane.goto(x_pos,y_pos-SQUARE_SIZE)
-    
-        
-    my_pos=plane.pos()
-   
-    
-    
-    
-           
+
+    while end_time-time.time()>0:
+        print(int(end_time-time.time()))
+        clear_score.goto(400, 0)
+        ax = clear_score.stamp()
+        s_l_s.append(ax)
+        clear_score.goto(800, 0)
+        turtle.write(int(end_time-time.time()),font=50)
         
 
 
-
-    new_pos=plane.pos()
-    new_x_pos=new_pos[0]
-    new_y_pos=new_pos[1]
-    
-    if new_x_pos>=RIGHT_EDGE:
-        print("GAME OVER !!! YOU HIT THE RIGHT EDGE ")
-        quit()
-    if new_x_pos<=LEFT_EDGE:
-        print("GAME OVER !!! YOU HIT THE LEFT EDGE ")
-        quit()
-    if new_y_pos<=DOWN_EDGE:
-        print("GAME OVER !!! YOU HIT THE LOWER EDGE ")
-        quit()
-    if new_y_pos>=UP_EDGE:
-        print("GAME OVER !!! YOU HIT THE UPPER EDGE ")
-        quit()
-   
-    
-    
-move_snake()
-
+        if direction==RIGHT:
+            plane.goto(x_pos+SQUARE_SIZE,y_pos)
+            print('you moevd right ')
+        if direction==DOWN:
+            plane.goto(x_pos,y_pos-SQUARE_SIZE)
+            print('you moevd DOWN ')
+        if direction==LEFT:
+            plane.goto(x_pos-SQUARE_SIZE,y_pos)
+            print('you moevd LEFT ')
+        if direction==UP:
+            plane.goto(x_pos,y_pos+SQUARE_SIZE)
+            print('you moevd UP ')
+        
+            
